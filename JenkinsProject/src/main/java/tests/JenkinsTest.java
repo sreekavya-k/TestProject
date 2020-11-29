159c6d7459987e83081484c522cb13fec98c8b07
Sony <Sony@Sony-PC>
2020-11-29 15:49:59 +0530

    first commit

---
+++ JenkinsProject/src/main/java/tests/JenkinsTest.java
@@ -0,0 +1,47 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class JenkinsTest {
+public WebDriver driver;
+@Test
+public void openBlog() {
+	driver.get("https://www.softwaretestingmaterial.com");
+}
+
+@BeforeClass
+public void beforeClass() {
+	System.setProperty("webdriver.gecko.driver", "E:\\Library\\geckodriver-v0.28.0-win64\\geckodriver.exe");
+	driver = new FirefoxDriver();
+}
+
+@AfterClass
+public void afterClass() {
+	driver.quit();
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
